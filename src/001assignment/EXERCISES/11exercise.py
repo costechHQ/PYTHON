bacd3e0b4ea6959ea00ec4a15d9d2e132f49b5c4
Onyedika Christopher <cosimon.tech@gmail.com>
2026-08-18 02:45:39 +0100

    progress

---
+++ src/001assignment/EXERCISES/11exercise.py
@@ -0,0 +1,9 @@
+a = 10
+b = 4  
+
+a, b = 10, 4
+print(f"Addition: {a + 5}")      
+print(f"Subtraction: {a - b}")    
+print(f"Multiplication: {a * 5}") 
+print(f"Division: {10 / 4}")       
+print(f"Modulus: {10 % 5}")        

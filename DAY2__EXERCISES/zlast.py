23480ea881651d6faa047215d003d3c04f1b1bd1
Onyedika Christopher <cosimon.tech@gmail.com>
2026-08-20 10:16:40 +0100

    done

--- DAY2__EXERCISES/zlast.py
+++ DAY2__EXERCISES/zlast.py
@@ -7,4 +7,4 @@ def two_sum(nums, target):
         seen_numbers[current_num] = index
 print("Example 1 Output:", two_sum([2, 7, 11, 15], 9))
 print("Example 2 Output:", two_sum([3, 2, 4], 6))
-print("Example 3 Output:", two_sum([3, 3], 6))     
+print("Example 3 Output:", two_sum([3, 3], 6))

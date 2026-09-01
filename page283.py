89056bccfd744127cf9ef9f6c3bd457f8dd70e77
Onyedika Christopher <cosimon.tech@gmail.com>
2026-09-01 05:44:55 +0100

    feat:  9-3 page283

--- page283.py
+++ page283.py
@@ -31,7 +31,36 @@ restaurant2.describe_restaurant()
 print()
 restaurant3.describe_restaurant()
 
-print("\n" + "="*30 + "\n")
 
 
+class User:
+    """class for user"""
+    def __init__(self, first_name, last_name, age, occupation):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.occupation = occupation
 
+    def describe_user(self):
+        """summary of the user’s information"""
+
+        print("\n--- Users details ----")
+        print(f"\nName: {self.first_name} {self.last_name}")
+        print(f"Age {self.age}")
+        print(f"Occupation: {self.occupation}")
+
+    def greet_user(self):
+        """personalized greeting to the user"""
+        print(f"\nHello, {self.first_name}, thanks you for logging in today")
+
+user1 = User("Onyedika", "Christopher", 30, "Software Engineer")
+user1.describe_user()
+user1.greet_user()
+
+user2 = User("Ifeanyi", "Charlse", 28, "Software Engineer")
+user2.describe_user()
+user2.greet_user()
+
+user3 = User("Loveth", "black", 26, "Software Engineer")
+user3.describe_user()
+user3.greet_user()
